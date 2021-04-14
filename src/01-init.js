1327dfad1839b64e16f666179f8237b05e7e148f
Sameer Dewan <sameer.dewan@itential.com>
2021-04-14 10:45:23 -0400

    add tests

---
+++ src/01-init.js
@@ -0,0 +1,15 @@
+'use strict';
+const adapters = require('@poetry/adapters');
+
+class Init {
+    constructor() {
+        return async (event) => {
+
+        };
+    }
+    initializeLambda() {
+        
+    }
+}
+
+exports.handler = new Init();
