c6adbe899e2af11266a8ebb3225a8c571c3c347b
Sameer Dewan <sameer.dewan@itential.com>
2021-04-14 10:45:53 -0400

    sinon mongoose stubs

--- src/01-init.js
+++ src/01-init.js
@@ -1,14 +1,32 @@
 'use strict';
 const adapters = require('@poetry/adapters');
+const {connectDB} = require('@poetry/mongoose');
+const {Project, Folder, Organization, File} = require('@poetry/mongoose').schemas;
 
 class Init {
     constructor() {
         return async (event) => {
-
+            try {
+                this.initializeLambda(event);
+                await connectDB();
+            } catch (error) {
+                if (this.attempts <= 5) {
+                    await this.reattempt();
+                } else {
+                    await this.abort();
+                }
+            }
         };
     }
-    initializeLambda() {
-        
+    initializeLambda({body}) {
+        this.hash = body.hash;
+        this.projectId = body.projectId;
+        this.folderId = body.folderId;
+        this.organizationId = body.organizationId;
+        this.subscriptionKey = body.subscriptionKey;
+        this.attempts = body.attempts ? body.attempts : 0;
+        this.network = body.network;
+        this.adapter = adapters[body.network];
     }
 }
 
