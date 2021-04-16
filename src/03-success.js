459a8cab2546b946fd1d3b7d87d7007da7a300b5
Sameer Dewan <sameer.dewan@itential.com>
2021-04-16 12:28:21 -0400

    success lambda

--- src/03-success.js
+++ src/03-success.js
@@ -1,9 +1,24 @@
 'use strict';
+const {File} = require('@poetry/mongoose').schemas;
 const Poetry = require('./utils/lambda');
 
 class Success extends Poetry.Lambda {
     async runLambda() {
-        
+        await this.connectDB();
+        await this.updateFile();
+    }
+    async updateFile() {
+        await File.updateOne(
+            { name: this.fileName, hash: this.hash, networks: { $elemMatch: { name: this.network } } },
+            { 
+                $set: { 
+                    'networks.$.contract': this.adapter.getAbiAndAddress().address, 
+                    'networks.$.tx': this.tx, 
+                    'networks.$.status': 'completed', 
+                    'networks.$.onChainDate': new Date() 
+                } 
+            }
+        ).exec();
     }
 }
 
